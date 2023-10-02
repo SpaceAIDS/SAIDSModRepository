52412d2778fbcfa0d9b264ca57b81b4fe564006a
RELIKT <maximilianoleaf@icloud.com>
2023-10-02 13:17:21 -0700

    HeavyAssaultMissileLauncher
    battlecruiser-intended weapon, fires salvo of 6 slow-moving missiles with a 50m proximity fuze. prone to missing against smaller targets, unlikely to against larger ones.
    added to weapon sheet guided spec at  600 points

--- KAALAKIOTA/Data/Scripts/CoreParts/MasterConfig.cs
+++ KAALAKIOTA/Data/Scripts/CoreParts/MasterConfig.cs
@@ -10,7 +10,7 @@ namespace Scripts
             // Enable your definitions using the follow syntax:
             // PartDefinitions(Your1stDefinition, Your2ndDefinition, Your3rdDefinition);
             // PartDefinitions includes both weapons and phantoms
-            PartDefinitions(Coil250,HeavyPulse,QuadArtilleryBase,QuadBarrelAutocannon,B8T_Flare,HeavyTorpedoLauncher,DualBarrelArtillery,PrototypeSiegeArtillery,LightMissileLauncher);
+            PartDefinitions(Coil250,HeavyPulse,QuadArtilleryBase,QuadBarrelAutocannon,B8T_Flare,HeavyTorpedoLauncher,DualBarrelArtillery,PrototypeSiegeArtillery,LightMissileLauncher,HeavyAssaultMissileLauncher);
             ArmorDefinitions();
             SupportDefinitions();
             UpgradeDefinitions();
